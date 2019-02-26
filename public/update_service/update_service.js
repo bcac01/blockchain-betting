d93c9f8cd7c6133d087211e0d18ea7004c4cf41f
Velja <velja@timacum.com>
2019-02-26 21:23:58 +0100

    set default timezone for update service

--- public/update_service/update_service.js
+++ public/update_service/update_service.js
@@ -8,6 +8,8 @@ const nodeUrl = require('../../src/eth-node-config'),
 	contractAddress = compiledContract.networks['300'].address,
 	contractInstance = new web3.eth.Contract(compiledContract.abi, contractAddress);
 
+moment.tz.setDefault("Europe/Belgrade");
+
 let coinbaseAddress = '',
 	availableAddresses = 0,
 	creatingAddress = false,
